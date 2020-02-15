03c8be1474646238aab1e1d1e6fbed5dee4c4a2a
abhayrm20 <abhayrm20@gmail.com>
2020-02-16 00:36:33 +0530

    Face rec is working!! Yaayyy!!!!

--- search.py
+++ search.py
@@ -1,14 +1,15 @@
 import glob
 import os
-def search(dir):
+def search(dir,fname):
     os.chdir(dir )
+    
     try:
         for files in glob.glob( "*.*" ):
             f = open( files, 'r' )
             file_contents = f.read()
             #if "AIzaSyAbg4XZm-68VmFtLnKWSAnNanO1oNTukT4" in file_contents:
             #if "AIzaSyDkM2fJKHovUnCx5AihUbQc136XgGutASc" in file_contents:
-            if "getPoints()" in file_contents:
+            if fname in file_contents:
                     print
                     print "*********************************************************************************"
                     print dir,"/", f.name
@@ -20,9 +21,10 @@ def search(dir):
         print "dir error"
 
 dir =  os.getcwd()
+fname = raw_input("Enter search keyword: ")
 print "Iterating root directory"
-search(dir)
+search(dir,fname)
 for x in os.walk(dir):
     dir = x[0]
     #print "Iterating dir: ", dir
-    search(dir)
+    search(dir,fname)
