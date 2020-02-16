16aaf143e65d3cf22d6c21ba4ee5aac153ddc287
abhayrm20 <abhayrm20@gmail.com>
2020-02-16 10:50:56 +0530

    Final push

--- nullcrew/settings.py
+++ nullcrew/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,4 +122,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
-django_heroku.settings(locals())
